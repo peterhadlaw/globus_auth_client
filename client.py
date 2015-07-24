caac84cd9943f2be9f209a6c141bfc3ba097c9b2
Peter Hadlaw <hadlawp@gmail.com>
2015-07-24 17:01:24 -0500

    Added required redirect_uri to auth

--- client.py
+++ client.py
@@ -2,6 +2,7 @@ from os import environ
 from flask import Flask
 from flask import render_template
 from flask import redirect
+from flask import url_for
 from oauth2client import client as oauth_client
 
 
@@ -17,7 +18,9 @@ def auth():
     scope = "profile"
     client_id = environ['OAUTH_CLIENT_ID']
     client_secret = environ['OAUTH_CLIENT_SECRET']
-    flow = oauth_client.OAuth2WebServerFlow(client_id, client_secret, scope)
+    redirect_uri = url_for("oauth2callback")
+    flow = oauth_client.OAuth2WebServerFlow(client_id, client_secret, scope,
+                                            redirect_uri = redirect_uri)
     auth_url = flow.step1_get_authorize_url()
     return redirect(location=auth_url)
 
