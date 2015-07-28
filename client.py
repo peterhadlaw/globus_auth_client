099f5ef2870523d0f538d28dbe24155aa086e3ab
Peter Hadlaw <hadlawp@gmail.com>
2015-07-28 14:54:12 -0500

    Condensed login err desc assignment

--- client.py
+++ client.py
@@ -37,18 +37,18 @@ def hello():
 
 @app.route("/profile")
 def profile():
+    flow = establishFlow()
+    auth_url = flow.step1_get_authorize_url()
     if "error" not in request.args and "code" not in request.args:
-        auth_url = establishFlow().step1_get_authorize_url()
         return render_template("need_login.html", auth_url=auth_url), 401
     if "error" in request.args:
         login_error = request.args.get("error")
-        login_error_desc = ""
-        if "error_description" in request.args:
-            login_error_desc = request.args.get("error_description")
+        login_error_desc = "" if "error_description" not in request.args else request.args.get("error_description")
         return render_template("login_error.html", login_error=login_error,
-                               login_error_desc=login_error_desc), 401
+                               login_error_desc=login_error_desc,
+                               auth_url=auth_url), 401
     auth_code = request.args.get('code')
-    result = establishFlow().step2_exchange(auth_code)
+    result = flow.step2_exchange(auth_code)
     return render_template("profile.html")
 
 
