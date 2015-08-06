0c87ff38bfaac3ead12ff7373ce0b0d163db26b0
Peter Hadlaw <hadlawp@gmail.com>
2015-08-06 15:49:16 -0500

    Added test auth url added to btn

--- goauth_client/tests/test_auth.py
+++ goauth_client/tests/test_auth.py
@@ -10,10 +10,18 @@ class GOAuthClientAuthTest(unittest.TestCase):
 
     def setUp(self):
         client.app.config['TESTING'] = True
+        # Needed to test `url_for`
+        client.app.config['SERVER_NAME'] = 'localhost'
         self.app = client.app.test_client()
 
     def tearDown(self):
         pass
+
+    def test_auth_url_added_to_button(self):
+        rv = self.app.get('/')
+        soup = BeautifulSoup(rv.data, 'html.parser')
+        auth_url = client.establishFlow().step1_get_authorize_url()
+        assert soup.find('a', class_="btn-primary")['href'] == auth_url
 
 if __name__ == '__main__':
     unittest.main()
