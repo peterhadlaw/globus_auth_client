134f379ed60206e3046ecd27f8e9a3b7301d11ec
Peter Hadlaw <hadlawp@gmail.com>
2015-08-06 14:23:45 -0500

    Added page title tests

--- goauth_client/tests/test_client.py
+++ goauth_client/tests/test_client.py
@@ -1,5 +1,9 @@
+from __future__ import print_function
+
 from goauth_client import client
 import unittest
+from bs4 import BeautifulSoup
+
 
 
 class GOAuthClientHelloTest(unittest.TestCase):
@@ -11,9 +15,15 @@ class GOAuthClientHelloTest(unittest.TestCase):
     def tearDown(self):
         pass
 
-    def test_home_heading(self):
+    def test_page_titles(self):
+        # Check the base template title string
         rv = self.app.get('/')
-        pass
+        soup = BeautifulSoup(rv.data, 'html.parser')
+        assert soup.title.string.endswith(' - Globus Auth Client Example')
+
+        # Check that each individual page has it's respective title.
+        assert soup.title.string.startswith('Home')
+
 
 if __name__ == '__main__':
     unittest.main()
