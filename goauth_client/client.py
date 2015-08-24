2c258fbd97687182e8cf001cb9e1b04fde6181c7
Peter Hadlaw <hadlawp@gmail.com>
2015-08-24 10:52:11 -0500

    Added API Explorer

--- goauth_client/client.py
+++ goauth_client/client.py
@@ -74,13 +74,23 @@ def profile():
         return render_template("profile.html", profile_data=result.id_token)
 
 
-@app.route("/profile/api_expo")
+@app.route("/profile/api_expo", methods=['GET', 'POST'])
 def profile_api_expo():
     if 'access' not in session:
         return redirect(url_for('profile'))
     else:
         url = "https://auth.api.globusauthtest.globuscs.info"
         headers = {'Authorization': 'Bearer {}'.format(session['access'])}
+
+        if request.method == 'POST':
+            # API Explorer was used, let's process the request
+            path = request.form['path'] # required
+            body = request.form.get(body, "")
+            method = request.form['method']
+            test_api_explorer = json.dumps(json.loads(requests.request(method, url + path, data=body, verify=False,
+                             headers=headers).text), indent=2)
+        else:
+            test_api_explorer = "No request processed. Please use API explorer."
 
         token_details_path = "/token_details"
         test_token_details = requests.get(url + token_details_path, verify=False,
@@ -94,7 +104,8 @@ def profile_api_expo():
             'token_details': json.dumps(json.loads(test_token_details.text),
                                         indent=2),
             'identities': json.dumps(json.loads(test_identities.text),
-                                     indent=2)
+                                     indent=2),
+            'test_api_explorer': test_api_explorer
         }
 
         return render_template("profile_api_expo.html", test_results=test_results)
