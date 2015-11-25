2fc4dca8a30702ee157300e28d4d63a5f415e80a
Stephen Rosen <sirosen@uchicago.edu>
2015-11-24 19:43:32 -0600

    Fix setup.py to capture all static files

--- setup.py
+++ setup.py
@@ -1,10 +1,21 @@
 import os
 from setuptools import setup, find_packages
 
+
+pkg_dir = os.path.join(os.path.dirname(__file__), 'globusauth_client')
+static_dir = os.path.join(pkg_dir, 'static')
+
+
+def all_static_files():
+    return reduce(lambda x, y: x+y,
+                  [[os.path.join(d, f)[len(pkg_dir)+1:] for f in fs]
+                   for (d, sds, fs) in os.walk(static_dir)])
+
 setup(
     name='globusauth_client',
     include_package_data=True,
-    package_data={'': ['*']},
+    package_data={'': ['templates/*.html'] + all_static_files()},
     packages=find_packages(),
-    package_dir={'': '.'}
+    package_dir={'': '.'},
+    zip_safe=False
 )
